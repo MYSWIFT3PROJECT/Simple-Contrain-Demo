684f22931cc023629ebb96a3c3d12f362cb20979
Nem Sothea <NemSothea@youvita-imac.local>
2017-03-01 11:40:36 +0700

    Nearly finish

---
+++ SimpleConstrain/MyCustomize.swift
@@ -0,0 +1,12 @@
+//
+//  MyCustomize.swift
+//  SimpleConstrain
+//
+//  Created by Nem Sothea on 3/1/17.
+//  Copyright © 2017 Nem Sothea. All rights reserved.
+//
+
+import UIKit
+class MyCustomize : UIView{
+        
+}
